d8926c7df0c0cd1a50c8244fc63b98f45a1826f5
Thomas Winwood <twwinwood@gmail.com>
2024-10-17 13:13:03 +0100

    Label data from cleartonic's spreadsheet

--- data/gate_info.asm
+++ data/gate_info.asm
@@ -1,43 +1,45 @@
-; A: unknown
-; B: unknown
-; C: unknown
+; A: "terrain type"
+; B: "special floors"
+; C: "item type"
 ; D: number of floors
 ; E: unknown
 ; F: unknown
 ; G: unknown
-; H: unknown
+; H: "money type"
+
+; TODO: ask cleartonic what the quoted columns mean
 
 gGateInfoTable::
     ;  A    B    C    D    E    F    G    H
-    db $00, $00, $00, $05, $30, $07, $02, $01   ; Beginning
-    db $01, $01, $01, $05, $31, $01, $06, $01   ; Villager
-    db $01, $01, $02, $06, $32, $05, $01, $01   ; Talisman
-    db $02, $01, $02, $05, $33, $04, $06, $01   ; Memories
-    db $02, $02, $03, $06, $34, $00, $07, $01   ; Bewilder
-    db $03, $02, $03, $09, $35, $01, $06, $01   ; Bazaar #1
-    db $03, $02, $04, $08, $36, $05, $01, $02   ; Peace
-    db $03, $03, $04, $09, $37, $05, $07, $02   ; Bravery
-    db $04, $03, $05, $0C, $38, $08, $03, $02   ; Well
-    db $04, $04, $05, $0B, $39, $02, $01, $02   ; Strength
-    db $04, $04, $05, $0B, $3C, $02, $06, $02   ; Anger
-    db $04, $04, $06, $0C, $10, $08, $05, $02   ; Monster Farm
-    db $05, $06, $06, $0E, $3B, $04, $01, $02   ; Joy
-    db $05, $06, $06, $0F, $3A, $01, $07, $02   ; Wisdom
-    db $05, $06, $07, $10, $3D, $04, $07, $02   ; Arena Left
-    db $06, $07, $07, $12, $3E, $04, $01, $03   ; Happiness
-    db $07, $07, $08, $14, $3F, $06, $03, $03   ; Temptation
-    db $07, $05, $08, $13, $40, $04, $06, $03   ; Medal Man
-    db $08, $08, $09, $17, $42, $04, $06, $03   ; Labyrinth
-    db $08, $09, $09, $19, $43, $05, $05, $03   ; Judgment
-    db $08, $05, $09, $19, $44, $00, $03, $03   ; Library
-    db $09, $0A, $0A, $1D, $45, $04, $07, $03   ; Reflection
-    db $0A, $0B, $0B, $1E, $46, $05, $06, $03   ; Ambition
-    db $0A, $0B, $0B, $1D, $47, $05, $06, $03   ; Demolition
-    db $0A, $0B, $0B, $1B, $48, $04, $07, $03   ; Mastermind
-    db $0B, $0C, $0C, $1E, $49, $04, $07, $03   ; Control
-    db $0B, $0C, $0C, $1E, $4A, $09, $07, $03   ; Extinction
-    db $0B, $0D, $0D, $1E, $4B, $04, $07, $03   ; Sleep
-    db $0C, $0D, $0D, $1E, $4C, $05, $05, $03   ; Bazaar #2
-    db $0D, $0E, $0E, $1B, $4D, $05, $07, $03   ; Arena Right
-    db $0E, $0E, $0E, $1E, $4E, $08, $0C, $03   ; Old Man
-    db $0F, $0F, $0F, $63, $4F, $05, $06, $03   ; Unused 0x1F
+    db $00, $00, $00,  5, $30, $07, $02,  1   ; Beginning
+    db $01, $01, $01,  5, $31, $01, $06,  1   ; Villager
+    db $01, $01, $02,  6, $32, $05, $01,  1   ; Talisman
+    db $02, $01, $02,  5, $33, $04, $06,  1   ; Memories
+    db $02, $02, $03,  6, $34, $00, $07,  1   ; Bewilder
+    db $03, $02, $03,  9, $35, $01, $06,  1   ; Bazaar #1
+    db $03, $02, $04,  8, $36, $05, $01,  2   ; Peace
+    db $03, $03, $04,  9, $37, $05, $07,  2   ; Bravery
+    db $04, $03, $05, 12, $38, $08, $03,  2   ; Well
+    db $04, $04, $05, 11, $39, $02, $01,  2   ; Strength
+    db $04, $04, $05, 11, $3C, $02, $06,  2   ; Anger
+    db $04, $04, $06, 12, $10, $08, $05,  2   ; Monster Farm
+    db $05, $06, $06, 14, $3B, $04, $01,  2   ; Joy
+    db $05, $06, $06, 15, $3A, $01, $07,  2   ; Wisdom
+    db $05, $06, $07, 16, $3D, $04, $07,  2   ; Arena Left
+    db $06, $07, $07, 18, $3E, $04, $01,  3   ; Happiness
+    db $07, $07, $08, 20, $3F, $06, $03,  3   ; Temptation
+    db $07, $05, $08, 19, $40, $04, $06,  3   ; Medal Man
+    db $08, $08, $09, 23, $42, $04, $06,  3   ; Labyrinth
+    db $08, $09, $09, 25, $43, $05, $05,  3   ; Judgment
+    db $08, $05, $09, 25, $44, $00, $03,  3   ; Library
+    db $09, $0A, $0A, 29, $45, $04, $07,  3   ; Reflection
+    db $0A, $0B, $0B, 30, $46, $05, $06,  3   ; Ambition
+    db $0A, $0B, $0B, 29, $47, $05, $06,  3   ; Demolition
+    db $0A, $0B, $0B, 27, $48, $04, $07,  3   ; Mastermind
+    db $0B, $0C, $0C, 30, $49, $04, $07,  3   ; Control
+    db $0B, $0C, $0C, 30, $4A, $09, $07,  3   ; Extinction
+    db $0B, $0D, $0D, 30, $4B, $04, $07,  3   ; Sleep
+    db $0C, $0D, $0D, 30, $4C, $05, $05,  3   ; Bazaar #2
+    db $0D, $0E, $0E, 27, $4D, $05, $07,  3   ; Arena Right
+    db $0E, $0E, $0E, 30, $4E, $08, $0C,  3   ; Old Man
+    db $0F, $0F, $0F, 99, $4F, $05, $06,  3   ; Unused 0x1F
